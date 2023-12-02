6ecc765e1ad394c1609251b9a10b8493598f7605
David Robertsson <david.robertsson@gmail.com>
2023-12-02 21:08:00 +0100

    Initial commit - Solution day 1

---
+++ lib/aoc.ex
@@ -0,0 +1,31 @@
+defmodule Aoc do
+  @moduledoc """
+  Documentation for `Aoc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  @path "./resources/adventofcode.com_2023_day_1_input.txt"
+  def day1() do
+    file_stream = File.stream!(@path, [], :line)
+
+    file_stream
+    |> Enum.reduce([], fn line, acc ->
+      [String.replace(line, ~r/[^\d]/, "") |> String.split("", trim: true) | acc]
+    end)
+    |> Enum.map(fn line -> [Enum.at(line, 0), Enum.at(line, -1)] end)
+    |> Enum.map(fn [h, l] -> String.to_integer("#{h}#{l}") end)
+    |> Enum.sum()
+  end
+end
