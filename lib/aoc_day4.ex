3d17b3f7c0a62ad05bba7c4ec4f03df87837be01
David Robertsson <david.robertsson@gmail.com>
2023-12-04 15:07:19 +0100

    Day 4 - Part 1
    tests and input data

---
+++ lib/aoc_day4.ex
@@ -0,0 +1,43 @@
+defmodule Aoc.Day4 do
+  @path_day4 "./resources/adventofcode.com_2023_day_4_input.txt"
+  def run() do
+    file_stream = File.stream!(@path_day4, [], :line)
+    solution(file_stream)
+  end
+
+  def solution(data) do
+    data
+    |> Enum.map(fn line -> line |> String.split(":") |> Enum.at(-1) end)
+    |> Enum.map(fn line -> line |> String.split("|") end)
+    |> Enum.map(fn [card, winners] ->
+      {card
+       |> String.split(~r/[^\d]/, trim: true)
+       |> Enum.map(fn x ->
+         String.to_integer(x)
+       end),
+       winners
+       |> String.split(~r/[^\d]/, trim: true)
+       |> Enum.map(fn x ->
+         String.to_integer(x)
+       end)}
+    end)
+    |> get_intersections()
+    |> Enum.map(fn set -> calc_powers_of_size(set) end)
+    |> Enum.sum()
+  end
+
+  def get_intersections(sets) do
+    sets
+    |> Enum.map(fn {card, winners} ->
+      MapSet.intersection(MapSet.new(card), MapSet.new(winners))
+    end)
+  end
+
+  def calc_powers_of_size(set) do
+    if Enum.count(set) > 0 do
+      2 ** (Enum.count(set) - 1)
+    else
+      0
+    end
+  end
+end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -103,4 +103,16 @@ defmodule AocTest do
     assert Aoc.Day3.solution_3(data2 |> String.split("\n", trim: true)) == 23354
     assert Aoc.Day3.day3() == 553_825
   end
+
+  test "day4_part1" do
+    data = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+
+    assert Aoc.Day4.solution(data |> String.split("\n", trim: true)) == 13
+    assert Aoc.Day4.run() == 25183
+  end
 end
